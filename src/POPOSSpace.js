9eea30ed826f9fa69bc0353be3f155724d7922a3
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-04-28 11:28:45 -0500

    added json data and hours

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './POPOSSpace.css'
 function POPOSSpace(props) {
-    const { name, image, address } = props
+    const { name, image, address,hours } = props
     return (
       <div>
         <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
         <h1>{name}</h1>
         <div>{address}</div>
+        <div>{hours}</div>
       </div>
     )
   }
