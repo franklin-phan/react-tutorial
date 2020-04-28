67f90a4a51ccaf4836999d64964d2e81273131cb
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-04-27 21:22:57 -0500

    first commit and built header component

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+      <div className="Title">
+        <header>
+          <h1>SFPOPOS</h1>
+        </header>
+      </div>
+    )
+}
+
+export default Title
